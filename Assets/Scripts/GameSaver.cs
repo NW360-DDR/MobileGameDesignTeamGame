f71fd580edc05bc9b2f1c431ed8641cc88b46508
Nathaniel Owens <rushlord42@gmail.com>
2022-10-23 13:54:37 -0400

    Save Script Now Functions
    The Script designed to save the game has now been implemented and confirmed working. Saves the "Last Meaningful Login" (to be determined as when the player actually does something to care for the friend) as a string, and when loading, parses the string into a proper DateTime object for the code to read. Documentation on this script exists inside the script to allow for easy reading and public access outside of the script.

--- Assets/Scripts/GameSaver.cs
+++ Assets/Scripts/GameSaver.cs
@@ -3,42 +3,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class GameSaver : MonoBehaviour
 {
+    // PUBLIC VARIABLES: These can be accessed from anywhere, like in a minigame script.
     public int wheatCount;
     public int friendship;
-    string lastMeaningfulLogin;
-    DateTime tempForLogin;
-    string currentLoginTime;
     public bool hasFedFriend;
-    // Start is called before the first frame update
+    // PRIVATE VARIABLES: These can only be read and used inside this script.
+    DateTime MeaningfulLogin;
+    DateTime CurrentLogin;
+    // Start is called before the first frame update.
+    // Start will call the Load function, and pull the current time for the user.
+    // Later versions plan to include a check to see how long since the last play.
     void Start()
     {
-        lastMeaningfulLogin = PlayerPrefs.GetString("Meaningful");
+        Load();
+        CurrentLogin = DateTime.Now;
+        Debug.Log("Current Time is: " + CurrentLogin.ToString());
+    }
+    //Saves the values such as Wheat, Friendship, and Meaningful.
+    // To add more to the Save function, it either needs to be an int, a float, or
+    // converted to a string by something like a ToString() command.
+    // Example: PlayerPrefs.SetFloat("PoundsOfBeef", BeefAmount);
+    public void Save()
+    {
+        if (hasFedFriend)
+        {
+            MeaningfulLogin = DateTime.Now;
+        }
+        PlayerPrefs.SetString("Meaningful", MeaningfulLogin.ToString());
+        PlayerPrefs.SetInt("Wheat", wheatCount);
+        PlayerPrefs.SetInt("Friendship", friendship);
+        PlayerPrefs.Save();
+        Debug.Log("Game Saved at " + MeaningfulLogin);
+    }
+    //Similarly, loads everything into the proper values.
+    // When converting from a string to another data type, make sure you know HOW to do it.
+    // DateTime has a built in function to parse it, but other data types may require
+    // a flat out typecast, like doubleVar = (double) PlayerPrefs.GetString("doubleVar");
+    public void Load()
+    {
+        MeaningfulLogin = DateTime.Parse(PlayerPrefs.GetString("Meaningful"));
+        // This debug log exists to make sure that parsing worked properly.
+        Debug.Log(MeaningfulLogin.ToString());
         wheatCount = PlayerPrefs.GetInt("Wheat");
         friendship = PlayerPrefs.GetInt("Friendship");
-        tempForLogin = DateTime.Now;
-        currentLoginTime = tempForLogin.ToString();
-        Debug.Log(tempForLogin.ToString("yyyy-MM-dd HH:mm:ss"));
-
-        SceneManager.sceneUnloaded += Save;
-    }
-
-    private void OnApplicationQuit()
-    {
-        PlayerPrefs.SetString("Meaningful", lastMeaningfulLogin);
-        PlayerPrefs.SetInt("Wheat", wheatCount);
-        PlayerPrefs.SetInt("Friendship", friendship);
-        PlayerPrefs.Save();
-    }
-
-
-    void Save<Scene>(Scene scene)
-    {
-        PlayerPrefs.SetString("Meaningful", lastMeaningfulLogin);
-        PlayerPrefs.SetInt("Wheat", wheatCount);
-        PlayerPrefs.SetInt("Friendship", friendship);
-        PlayerPrefs.Save();
     }
 }
